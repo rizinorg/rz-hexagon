365867b3a8cd6ce872aacadb45e2df3d46ca723c
Rot127 <unisono@quyllur.org>
2021-09-01 09:20:45 +0200

    -1 to UT64_MAX, Spelling, remove condition with no effect.

--- handwritten/analysis_hexagon_c/functions.c
+++ handwritten/analysis_hexagon_c/functions.c
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 static int hexagon_v6_op(RzAnalysis *analysis, RzAnalysisOp *op, ut64 addr, const ut8 *buf, int len, RzAnalysisOpMask mask) {
-	HexInsn hi = {0};;
+	HexInsn hi = {0};
 	ut32 data = 0;
 	memset (op, 0, sizeof (RzAnalysisOp));
 	data = rz_read_le32 (buf);
@@ -14,7 +14,7 @@ static int hexagon_v6_op(RzAnalysis *analysis, RzAnalysisOp *op, ut64 addr, cons
 	}
 
 	op->addr = addr;
-	op->jump = op->fail = -1;
-	op->ptr = op->val = -1;
+	op->jump = op->fail = UT64_MAX;
+	op->ptr = op->val = UT64_MAX;
 	return hexagon_analysis_instruction (&hi, op);
 }
