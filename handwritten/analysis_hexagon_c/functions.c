dd17f89d6967f9f6eb76aec92a724538864f3246
Rot127 <unisono@quyllur.org>
2021-09-29 15:20:57 +0200

    Remove emtpty line in license header.

--- handwritten/analysis_hexagon_c/functions.c
+++ handwritten/analysis_hexagon_c/functions.c
@@ -1,5 +1,4 @@
 // SPDX-FileCopyrightText: 2021 Rot127 <unisono@quyllur.org>
-//
 // SPDX-License-Identifier: LGPL-3.0-only
 
 static int hexagon_v6_op(RzAnalysis *analysis, RzAnalysisOp *op, ut64 addr, const ut8 *buf, int len, RzAnalysisOpMask mask) {
