84810004d805668fcc700aec4c9f801a350044d7
Rot127 <unisono@quyllur.org>
2021-12-03 04:38:52 -0500

    Rename synteax_post/prefix to mnem_post/prefix
    - Fix CID 365806 by increasing buffer by 8 bytes

--- LLVMImporter.py
+++ LLVMImporter.py
@@ -417,8 +417,8 @@ class LLVMImporter:
                 "if (hi->instruction == HEX_INS_INVALID_DECODE) {\n"
                 + "hi->parse_bits = ((hi_u32) & 0xc000) >> 14;\n"
                 + 'sprintf(hi->mnem_infix, "invalid");\n'
-                + 'sprintf(hi->mnem, "%s%s%s", hi->pkt_info.syntax_prefix,'
-                " hi->mnem_infix, hi->pkt_info.syntax_postfix);\n" + "}\n" + "return 4;\n}"
+                + 'sprintf(hi->mnem, "%s%s%s", hi->pkt_info.mnem_prefix,'
+                " hi->mnem_infix, hi->pkt_info.mnem_postfix);\n" + "}\n" + "return 4;\n}"
             )
             dest.write(main_function)
         log(
