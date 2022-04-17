9343c158ad62d01110e8be951c7828395bc69548
Florian Märkl <info@florianmaerkl.de>
2022-04-17 16:49:57 +0200

    Fail safely when too few bytes are provided to disasm (#61)

--- handwritten/analysis_hexagon_c/functions.c
+++ handwritten/analysis_hexagon_c/functions.c
@@ -2,6 +2,10 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 RZ_API int hexagon_v6_op(RzAnalysis *analysis, RzAnalysisOp *op, ut64 addr, const ut8 *buf, int len, RzAnalysisOpMask mask) {
+	rz_return_val_if_fail(analysis && op && buf, -1);
+	if (len < 4) {
+		return -1;
+	}
 	if (analysis->pcalign == 0) {
 		analysis->pcalign = 0x4;
 	}
